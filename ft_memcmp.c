460837c1ca936048b79a8105d02e9e828b9e937c
Sacha Kock <kock.sacha@gmail.com>
2024-10-26 12:15:05 +0200

    sacha

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skock <skock@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,31 +12,7 @@
 
 #include "libft.h"
 
-char *ft_strchr(const char *str, int c)
+int	memcmp(const void *s1, const void *s2, size_t n)
 {
-	int	i;
 
-	i = 0;
-	if (str == NULL)
-		return (NULL);
-	while (str[i])
-	{
-		if (str[i] == c)
-			return ((char *)&str[i]);
-		i++;
-	}
-	if (str[i] == (char) c)
-		return ((char *)&str[i]);
-	return (NULL);
 }
-
-/*int	main(void)
-{
-	char str[] = "comment ca va lequipe";
-	char *ptr;
-	int i = 'a';
-
-	ptr = ft_strchr(str, i);
-	printf("il y a %c, dans %s, ici %s", i, str, ptr);
-	return 0;
-}/*
