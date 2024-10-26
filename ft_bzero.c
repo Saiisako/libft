98058da890d5e152eed3259a9ece49ac8f8db2dd
Sacha Kock <kock.sacha@gmail.com>
2024-10-26 11:54:25 +0200

    sacha

--- ft_bzero.c
+++ ft_bzero.c
@@ -17,9 +17,8 @@ void	ft_bzero(void *s, size_t n)
 	char		*ptr;
 	size_t	i;
 
-	*ptr = (char *)s;
+	ptr = (char *)s;
 	i = 0;
-
 	while (i < n)
 	{
 		*ptr++ = '\0';
--- ft_calloc.c
+++ ft_calloc.c
@@ -10,11 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*calloc(size_t nmemb, size_t size)
-{
+#include "libft.h"
 
-}
-int	main(void)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
+	void	*ptr;
 
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * nmemb);
+	return (ptr);
 }
+
+/*
+int main() {
+    // Test 1 : Allocation d'un tableau de 5 entiers
+    int *tableau = (int *)ft_calloc(5, sizeof(int));
+
+    printf("Test 1 : Vérification de l'initialisation à zéro pour un tableau d'entiers\n");
+    for (int i = 0; i < 5; i++) {
+        printf("tableau[%d] = %d\n", i, tableau[i]); // Devrait afficher 0 pour chaque élément
+    }
+
+    // Test 2 : Allocation d'une chaîne de 20 caractères
+    char *chaine = (char *)ft_calloc(20, sizeof(char));
+
+    if (chaine == NULL) {
+        printf("Échec de l'allocation de mémoire pour la chaîne de caractères\n");
+        return 1;
+    }
+
+    printf("\nTest 2 : Vérification de l'initialisation à zéro pour une chaîne de caractères\n");
+    printf("Chaîne initialisée : '%s'\n", chaine); // Devrait afficher une chaîne vide
+
+    // Remplit la chaîne avec un mot pour tester
+    strcpy(chaine, "Bonjour");
+    printf("Chaîne après ajout : '%s'\n", chaine);
+
+    return 0;
+}
+/*
